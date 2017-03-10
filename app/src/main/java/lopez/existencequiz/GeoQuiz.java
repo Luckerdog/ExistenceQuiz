33ce3083c5f400784d346403279badebbe112a47
fccstudent <fresnocitystudent@gmail.com>
2017-03-09 20:26:03 -0800

    Night #1 Quiz

---
+++ app/src/main/java/lopez/existencequiz/GeoQuiz.java
@@ -0,0 +1,46 @@
+package lopez.existencequiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class GeoQuiz extends AppCompatActivity {
+
+    private Button mA, mB, mC, mD;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_geo_quiz);
+        mA = (Button) findViewById(R.id.button_A);
+        mA.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+        mB = (Button) findViewById(R.id.button_B);
+        mB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GeoQuiz.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+        mC = (Button) findViewById(R.id.button_C);
+        mC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+        mD = (Button) findViewById(R.id.button_D);
+        mD.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
