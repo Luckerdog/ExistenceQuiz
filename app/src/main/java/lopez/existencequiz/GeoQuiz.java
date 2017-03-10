b52a8fa8e9ad781f073e1223e20982573424819e
fccstudent <fresnocitystudent@gmail.com>
2017-03-09 21:03:27 -0800

    Updated buttons

--- app/src/main/java/lopez/existencequiz/GeoQuiz.java
+++ app/src/main/java/lopez/existencequiz/GeoQuiz.java
@@ -18,28 +18,36 @@ public class GeoQuiz extends AppCompatActivity {
         mA.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT);
+                toast.setGravity(10,5,5);
+                toast.show();
             }
         });
         mB = (Button) findViewById(R.id.button_B);
         mB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(GeoQuiz.this,R.string.correct_toast,Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(GeoQuiz.this,R.string.correct_toast,Toast.LENGTH_SHORT);
+                toast.setGravity(10,5,5);
+                toast.show();
             }
         });
         mC = (Button) findViewById(R.id.button_C);
         mC.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT);
+                toast.setGravity(10,5,5);
+                toast.show();
             }
         });
         mD = (Button) findViewById(R.id.button_D);
         mD.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show();
+                Toast toast = Toast.makeText(GeoQuiz.this,R.string.incorrect_toast,Toast.LENGTH_SHORT);
+                toast.setGravity(10,5,5);
+                toast.show();
             }
         });
     }
